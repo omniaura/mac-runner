9a583638ff40ed55ca6632e4082ca700e3efa91b
NanoClaw Agent <nanoclaw@users.noreply.github.com>
2026-02-10 01:57:58 +0000

    Convert to proper Homebrew cask format

---
+++ Casks/mac-runner.rb
@@ -0,0 +1,18 @@
+cask "mac-runner" do
+  version "0.1.0"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+
+  url "https://github.com/omniaura/mac-runner/releases/download/v#{version}/MacRunner-#{version}.zip"
+  name "Mac Runner"
+  desc "Menu bar app for managing GitHub Actions self-hosted runners"
+  homepage "https://github.com/omniaura/mac-runner"
+
+  depends_on macos: ">= :ventura"
+
+  app "MacRunner.app"
+
+  zap trash: [
+    "~/Library/Application Support/MacRunner",
+    "~/Library/Preferences/com.omniaura.mac-runner.plist",
+  ]
+end
