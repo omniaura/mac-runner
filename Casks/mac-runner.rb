636c730b80a0fd6d432ecea94245720f257b55c9
NanoClaw Agent <nanoclaw@users.noreply.github.com>
2026-02-10 01:58:16 +0000

    Update cask: add no_check and caveats for pre-release

--- Casks/mac-runner.rb
+++ Casks/mac-runner.rb
@@ -1,6 +1,6 @@
 cask "mac-runner" do
   version "0.1.0"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 :no_check  # Will be updated when first release is published
 
   url "https://github.com/omniaura/mac-runner/releases/download/v#{version}/MacRunner-#{version}.zip"
   name "Mac Runner"
@@ -15,4 +15,15 @@ cask "mac-runner" do
     "~/Library/Application Support/MacRunner",
     "~/Library/Preferences/com.omniaura.mac-runner.plist",
   ]
+
+  caveats <<~EOS
+    Mac Runner is in early development. To use:
+
+    1. Launch from Applications
+    2. Click the runner icon in menu bar
+    3. Add your GitHub repos with PAT token
+    4. Runners will be automatically configured!
+
+    Note: First release pending. The app will be available after v0.1.0 is published.
+  EOS
 end
